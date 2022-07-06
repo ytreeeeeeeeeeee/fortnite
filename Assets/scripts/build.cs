d61c1232c957941a7f2494e6a068b5b7e031d81c
ytreeeeeeeeeeee <106033849+ytreeeeeeeeeeee@users.noreply.github.com>
2022-07-07 01:12:12 +0300

    можно строить в воздухе все строения
    все немного багованое  но можно строиться в воздухе. строения можно поставить друг в друге(надо пофиксить)

--- Assets/scripts/build.cs
+++ Assets/scripts/build.cs
@@ -8,6 +8,8 @@ public class build : MonoBehaviour
     [SerializeField] GameObject floorEx;
     [SerializeField] GameObject wall;
     [SerializeField] GameObject wallEx;
+    [SerializeField] GameObject ramp;
+    [SerializeField] GameObject rampEx;
     
     GameObject example;
     string typeBuilding;
@@ -19,14 +21,17 @@ public class build : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Z))
         {
+            Destroy(example);
             typeBuilding = "floor";
         }
         else if (Input.GetKey(KeyCode.X))
         {
+            Destroy(example);
             typeBuilding = "wall";
         }
         else if (Input.GetKey(KeyCode.C))
         {
+            Destroy(example);
             typeBuilding = "ramp";
         }
         else if (Input.GetMouseButtonDown(1))
@@ -39,11 +44,14 @@ public class build : MonoBehaviour
             case "floor":
                 CreateFloor();
                 break;
-            case "nothing":
-                Destroy(example);
-                break;
             case "wall":
                 CreateWall();
+                break;
+            case "ramp":
+                CreateRamp();
+                break;
+            case "nothing":
+                Destroy(example);
                 break;
             default:
                 break;
@@ -55,11 +63,14 @@ public class build : MonoBehaviour
 
         if (Physics.Raycast(transform.position, transform.forward, out hit))
         {
+            print(hit.point);
+            print(Mathf.RoundToInt((hit.point.x + 0.4f) / 3) * 3);
+            print(Mathf.RoundToInt(hit.point.x / 3) * 3);
             if (Vector3.Distance(transform.position, hit.point) <= 6)
             {
-                Vector3 pos = new Vector3(Mathf.RoundToInt(hit.point.x) != 0 ? Mathf.RoundToInt(hit.point.x / 3) * 3 : 0, 
-                    (Mathf.RoundToInt(hit.point.y) != 0 ? Mathf.RoundToInt(hit.point.y / 3) * 3 : 0) + floorEx.transform.localScale.y / 2, 
-                    Mathf.RoundToInt(hit.point.z) != 0 ? Mathf.RoundToInt(hit.point.z / 3) * 3 : 0);
+                Vector3 pos = new Vector3(Mathf.Abs(Mathf.RoundToInt(hit.point.x / 3) * 3 - hit.point.x) > 0.35f ? (hit.point.x > 0 ? Mathf.RoundToInt((hit.point.x + 0.4f) / 3) * 3 : Mathf.RoundToInt((hit.point.x - 0.4f) / 3) * 3) : Mathf.RoundToInt(hit.point.x / 3) * 3, 
+                    (Mathf.RoundToInt(hit.point.y) != 0 ? Mathf.RoundToInt(hit.point.y / 3) * 3 : 0) + floorEx.transform.localScale.y / 2,
+                    Mathf.Abs(Mathf.RoundToInt(hit.point.z / 3) * 3 - hit.point.z) > 0.35f ? (hit.point.z > 0 ? Mathf.RoundToInt((hit.point.z + 0.4f) / 3) * 3 : Mathf.RoundToInt((hit.point.z - 0.4f) / 3) * 3) : Mathf.RoundToInt(hit.point.z / 3) * 3);
                 if (example == null)
                 {
                     example = Instantiate(floorEx, pos, Quaternion.identity);
@@ -88,7 +99,7 @@ public class build : MonoBehaviour
             if (Vector3.Distance(transform.position, hit.point) <= 6)
             {
                 Vector3 pos = new Vector3(Mathf.RoundToInt(hit.point.x) != 0 ? Mathf.RoundToInt(hit.point.x / 3) * 3 : 0,
-                    (Mathf.RoundToInt(hit.point.y) != 0 ? Mathf.RoundToInt(hit.point.y / 3) * 3 : 0) + wallEx.transform.localScale.y / 2,
+                    (Mathf.RoundToInt(hit.point.y) != 0 ? Mathf.RoundToInt(hit.point.y / 3) * 3 : 0) + 1.5f,
                     Mathf.RoundToInt(hit.point.z) != 0 ? Mathf.RoundToInt(hit.point.z / 3) * 3 : 0);
                 if (example == null)
                 {
@@ -107,6 +118,37 @@ public class build : MonoBehaviour
             if (Input.GetMouseButtonDown(0))
             {
                 Instantiate(wall, example.transform.position, example.transform.rotation);
+            }
+        }
+    }
+    void CreateRamp()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            if (Vector3.Distance(transform.position, hit.point) <= 6)
+            {
+                Vector3 pos = new Vector3(Mathf.Abs(Mathf.RoundToInt(hit.point.x / 3) * 3 - hit.point.x) > 0.35f ? (hit.point.x > 0 ? Mathf.RoundToInt((hit.point.x + 0.4f) / 3) * 3 : Mathf.RoundToInt((hit.point.x - 0.4f) / 3) * 3) : Mathf.RoundToInt(hit.point.x / 3) * 3,
+                    (Mathf.RoundToInt(hit.point.y) != 0 ? Mathf.RoundToInt(hit.point.y / 3) * 3 : 0) + 1.5f,
+                    Mathf.Abs(Mathf.RoundToInt(hit.point.z / 3) * 3 - hit.point.z) > 0.35f ? (hit.point.z > 0 ? Mathf.RoundToInt((hit.point.z + 0.4f) / 3) * 3 : Mathf.RoundToInt((hit.point.z - 0.4f) / 3) * 3) : Mathf.RoundToInt(hit.point.z / 3) * 3);
+                if (example == null)
+                {
+                    example = Instantiate(rampEx, pos, Quaternion.identity);
+                }
+                else
+                {
+                    example.transform.position = pos;
+                    example.transform.eulerAngles = new Vector3(-45, Mathf.RoundToInt(transform.eulerAngles.y) != 0 ? Mathf.RoundToInt(transform.eulerAngles.y / 90) * 90 : 0, 0);
+                }
+            }
+            else
+            {
+                Destroy(example);
+            }
+            if (Input.GetMouseButtonDown(0))
+            {
+                Instantiate(ramp, example.transform.position, example.transform.rotation);
             }
         }
     }
